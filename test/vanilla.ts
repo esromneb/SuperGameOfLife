fc4ea51a7aa79b603055839f16fc7c85d73ec3e2
Ben <bjmorse@gmail.com>
2020-09-12 16:46:25 -0700

    some sort of progress on tests, not working tho

--- test/vanilla.ts
+++ test/vanilla.ts
@@ -1,9 +1,25 @@
 import {WorldParent} from '../src/WorldParent'
 
+import { mocked } from 'ts-jest/utils';
 
-test('empty test', done => {
+// import jest from 'jest'
+
+const defaultOptions = {
+  width: 1920,
+  height: 1080,
+  transparent: false,
+  backgroundColor: 0x000000,
+};
+
+
+jest.mock('pixi.js');
+
+
+test('WorldParent builds', done => {
   console.log("test start");
 
-  expect(4).not.toBe(undefined);
+  const wp = new WorldParent({}, defaultOptions);
+
+  // expect(4).not.toBe(undefined);
   done();
 });
